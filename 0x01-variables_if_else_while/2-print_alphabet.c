81116caaaff69aea86a7c637a1ee78b731bd19bb
barbyleen <bmsaningo@gmail.com>
2022-10-17 07:08:10 -0700

    t2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - code using putchar
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch='a' ; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
+
