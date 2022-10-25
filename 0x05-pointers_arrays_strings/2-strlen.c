0509e965e0a879dc37f94382d5af91266fb93ea7
barbyleen <bmsaningo@gmail.com>
2022-10-25 09:55:45 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * __strlen - calculates the length of a string
+ *  @s: string
+ *
+ *  Return: length of the string
+*/
+int _strlen(char *s);
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len += 1;
+		*s = *s + 1;
+	}
+	return (len);
+}
