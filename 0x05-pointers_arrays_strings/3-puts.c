72443d9d1b7aa343c9d4f8cd7be542919c2c9054
barbyleen <bmsaningo@gmail.com>
2022-10-25 10:42:44 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to standard output
+ * @str: string pointer
+ *
+ * Return: 0
+*/
+
+void _puts(char *str)
+{
+	char *str;
+
+	_puts(*str);
+
+	return (0);
+}
