1405c79181c98a3135a9bce06da8f078eb633e26
barbyleen <bmsaningo@gmail.com>
2022-10-27 11:24:54 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates strings and ends with a null character
+ * @src: source
+ * @dest: pointer of resulting string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int b;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	b = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[b];
+		i++;
+		b++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
